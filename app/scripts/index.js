363c432e7923b9ea91018aece89e70132014886d
Oskar Rough <oskar@rough.dk>
2017-01-17 23:57:09 +0100

    First round

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -4,6 +4,8 @@ import $ from 'jquery';
 import ytPlayer from './youtube';
 import scPlayer from './soundcloud';
 
+// MODEL
+
 const videos = [
 	// 'eCIJZ_6Qcck',
 	// 'RHOy9XObpZU',
@@ -29,28 +31,42 @@ const videos = [
 
 const videoTheRhythm = 'DHTyG8x5UPo'
 
-const getRandomVideo = () => {
+const findRandomVideo = () => {
 	return videos[Math.floor(Math.random() * videos.length)]
 }
 
-ytPlayer.mute();
-// ytPlayer.loadVideoById(videos[0])
+// ACTIONS
 
-const remoteControl = document.querySelector('.RemoteControl')
+const play = function () {
+	// Only toggles volume.
+	scPlayer.toggle()
+	remoteControl.classList.add('is-playing')
+}
+
+const pause = function () {
+	// Only toggles volume.
+	scPlayer.toggle()
+	remoteControl.classList.remove('is-playing')
+}
 
 const changeTrack = function (event) {
 	let $el = $(event.currentTarget)
+
+	// Use the string number text to decide which track to play.
 	let index = Number($el.text().trim() - 1)
-	console.log(index)
+
 	// Change video
 	if (index === 4) {
 		ytPlayer.loadVideoById(videoTheRhythm)
 	} else {
-		ytPlayer.loadVideoById(getRandomVideo())
+		ytPlayer.loadVideoById(findRandomVideo())
 	}
+
 	// Change track.
 	scPlayer.skip(index)
 }
+
+// EVENT HANDLERS
 
 $('.js-volUp').on('click', () => {
 	scPlayer.getVolume(vol => {
@@ -71,25 +87,22 @@ $('.js-volDown').on('click', () => {
 $('.js-toggleSound').on('click', () => {
 	scPlayer.toggle()
 })
-
 $('.js-next').on('click', () => {
 	scPlayer.next()
 })
-
 $('.js-changeTrack').on('click', changeTrack)
 
-const play = function () {
-	scPlayer.toggle()
-	remoteControl.classList.add('is-playing')
+// START
+
+const start = function () {
+	// Mute the video.
+	ytPlayer.mute();
+
+	// TODO: Rewrite to wait for Soundcloud Widget API callback.
+	setTimeout(() => {
+		console.log('autoplaying first track')
+		$('.js-changeTrack').eq(0).trigger('click')
+	}, 1000)
 }
 
-const pause = function () {
-	scPlayer.toggle()
-	remoteControl.classList.remove('is-playing')
-	// ytPlayer.pauseVideo()
-}
-
-setTimeout(() => {
-	console.log('autoplaying first track')
-	$('.js-changeTrack').eq(0).trigger('click')
-}, 800)
+start()
